6a4715074fb89741df236912e957a0ce80429d43
Frederic Descamps <lefred.descamps@gmail.com>
2026-08-19 15:30:42 +0200

    fix version
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

--- plugin.cc
+++ plugin.cc
@@ -27,7 +27,7 @@ static struct st_mariadb_data_type plugin_descriptor_semver=
 
 #define SEMVER_PLUGIN_ENTRY(TYPE, DESCRIPTOR, NAME, DESCRIPTION) \
 { TYPE, DESCRIPTOR, NAME, "lefred", DESCRIPTION, PLUGIN_LICENSE_GPL, \
-  0, 0, 0x0100, NULL, NULL, "1.0.0", MariaDB_PLUGIN_MATURITY_BETA }
+  0, 0, 0x0100, NULL, NULL, "0.1.0", MariaDB_PLUGIN_MATURITY_BETA }
 
 maria_declare_plugin(type_semver)
   SEMVER_PLUGIN_ENTRY(MariaDB_DATA_TYPE_PLUGIN, &plugin_descriptor_semver,
